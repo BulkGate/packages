612c878abaaa883600f1fe9bf2b549e486e0181f
Lukáš Piják <lukaspijak@gmail.com>
2018-03-23 15:33:51 +0100

    Update example2.php

--- example2.php
+++ example2.php
@@ -7,7 +7,7 @@
 /** @var BulkGate\Sms\ISender $sender */
 $sender = require_once __DIR__.'/init.php';
 
-$message1 = new BulkGate\Sms\Message("420608422536", "test message");
+$message1 = new BulkGate\Sms\Message("420777777777", "test message");
 
 $message2 = new BulkGate\Sms\Message(
     new BulkGate\Sms\Message\PhoneNumber("777777777", BulkGate\Sms\Country::CZECH_REPUBLIC), "test message"
